951e7d8e4fc2d93ffe2cec8ba4371433406bbbd0
Hrushikesh <kukularehrushi@gmail.com>
2020-04-26 15:35:08 +0530

    Completed Login and SignUp Route

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser') 
+const {Pool} = require('pg')
+const bcrypt = require('bcrypt')
+
+const {addStudent,addStudentDetails,getStudent, getStudentByEmail} = require('./utils/queries')
 
 
 const app = express()
@@ -9,9 +13,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
+const db = new Pool({
+    user: "hrushi",
+    password: "hrushi",
+    host: "localhost",
+    port: 5432,
+    database: 'messManagement'
+})
 
-
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  await db.query(getStudentInfo())
     res.render('home')
 })
 
@@ -26,4 +37,54 @@ app.get('/signUp', (req, res) => {
 
 
 
+app.post('/login',async (req, res) => {
+    var student = {
+        email: req.body.email,
+        password:req.body.password
+    }
+    try {     
+    let result = await db.query(getStudentByEmail(student.email))
+    bcrypt.compare(student.password, result.rows[0].password).then((result) => {
+        goToHome(res)
+    })
+    } catch (error) {
+        console.log('error', error)  
+    }
+})
+
+
+app.post('/signUp', async function(req, res)  {
+    var student = {
+        email: req.body.email, 
+        name:req.body.name,
+        password:await bcrypt.hash(req.body.password, 10),
+        department:req.body.department,
+        year:req.body.year,
+        registration_id: req.body.registration_id,
+        mobile_no: req.body.mobile_no,
+        father_mobile_no: req.body.father_mobile_no,
+        mess_name: 'C Mess',
+        mess_id: 1
+    }
+    try {
+
+        let response = await db.query(addStudent(student))
+        console.log(response.rows)
+    let result =  await db.query(addStudentDetails(student,response.rows[0].id))
+    console.log(response.rows)
+    res.redirect('/')
+    } catch (error) {
+        console.log('error', error)
+    }
+  
+
+
+})
+
+
 app.listen(9000, () => console.log('Server started on port 9000'))
+
+
+const goToHome =async (res) => {
+db.query
+}
--- utils/queries.js
+++ utils/queries.js
@@ -1,7 +1,7 @@
 const {Pool } = require('pg')
 
 
-exports.getUserByEmail = (email) => {
+exports.getStudentByEmail = (email) => {
     return query = {
         text: 'SELECT * FROM students WHERE email = $1',
         values:[email]
@@ -20,5 +20,25 @@ exports.getStudentMessDetails = (student_id) => {
     return query = {
         text: 'SELECT * FROM mess_data WHERE student_id = $1',
         values:[student_id]
+    }
+}
+
+exports.addStudent = (student) => {
+    return query = {
+        text: 'INSERT INTO students(mess_name,mess_id,email,password, registration_id)  VALUES($1, $2, $3, $4, $5) RETURNING id ',
+        values:[student.mess_name, student.mess_id, student.email, student.password, student.registration_id]
+    }
+}
+
+exports.addStudentDetails = (student, id) => {
+    return query = {
+        text: 'INSERT INTO student_details(id,mess_id,name, department, year, mobile_no, fathers_mobile_no) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id',
+        values:[id, student.mess_id, student.name, student.department, student.year, student.mobile_no, student.father_mobile_no]
+    }
+}
+
+exports.getStudent = () => {
+    return query = {
+        text: "SELECT * FROM students"
     }
 }
